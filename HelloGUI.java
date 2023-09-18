22c1ce008425883c577631224549db76e51437e8
Felix Elias <felixelias5@gmail.com>
2023-09-18 07:49:10 -0400

    Removed unnecesary code

---
+++ HelloGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class HelloGUI extends JFrame {
+    public HelloGUI() {
+        super("Hello World with GUI"); //Set title
+        JFrame c = new JFrame("Hello, World!");
+        c.add(new JLabel());
+    }
+
+    public static void main(String[] args) {
+        HelloGUI window = new HelloGUI();
+        window.setSize(300, 100);
+        window.pack();
+        window.setVisible(true);
+    }
+}
